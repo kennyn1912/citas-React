1390b6081b1014cf3f189dc34c42f88ef46c5261
Kennyn Aullon <kennynaullon@gmail.com>
2023-02-25 12:26:27 -0500

    subiendo el proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+
+
+    return(
+        <h1 className="font-black text-5xl text-center  md:w-2/3 mx-auto">
+            <h1>Seguimiento Pacientes {''}
+            <spanc className="text-indigo-600">Veterinaria</spanc></h1>
+           
+        </h1>
+    )
+}
+export default Header;
